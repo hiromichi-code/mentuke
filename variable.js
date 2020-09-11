c66b1ebdb8677c5a97219f3515b18b896d1ce74a
hiromichi-code <hiromichi.s.617@gmail.com>
2020-09-11 12:43:48 +0900

    variable.js作成〜内容定義中

---
+++ variable.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const A4Height = 297;
+const A4Width = 210;
+const A5Height = 210;
+const A5Width = 148;
+const A6Height = 148;
+const A6Width = 105;
+const B5Height = 257;
+const B5Width = 182;
+const fourSixHeight = 188;
+const fourSixWidth = 128;
+const B6Height = 182;
+const B6Width = 128;
+
+const kAllLong = 939;
+const kAllShort = 636;
+const aAllLong = 880;
+const aAllShort = 625;
+const kHalfLong = 636;
+const kHalfShort = 469;
+const aHalfLong = 625;
+const aHalfShort = 440;
+const fourSixHalfLong = 788;
+const fourSixHalfShort = 545;
+const bHalfLong = 765;
+const bHalfShort = 542;
+
+
