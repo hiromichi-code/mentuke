b91a2fa98e442b9dcb4e3aa7565640b13d63a62b
hiromichi-code <hiromichi.s.617@gmail.com>
2020-09-16 12:43:03 +0900

    関数作業中

--- variable.js
+++ variable.js
@@ -1,37 +1,37 @@
 'use strict';
 
 const bookSizeHight = {
-  A4:297,
-  A5:210,
-  A6:148,
-  B5:257,
-  FourSix:188,
-  B6:182
+  A4:　297,
+  A5:　210,
+  A6:　148,
+  B5:　257,
+  FourSix:　188,
+  B6:　182
 }
 
 const bookSizeWidth = {
-  A4:210,
-  A5:148,
-  A6:105,
-  B5:182,
-  FourSix:128,
-  B6:128
+  A4:　210,
+  A5:　148,
+  A6:　105,
+  B5:　182,
+  FourSix:　128,
+  B6:　128
 }
 
 const paperSizeLong = {
-  kAll:939,
-  aAll:880,
-  kHalf:636,
-  aHalf:625,
-  FourSixHalf:788,
-  bHalf:765
+  kAll:　939,
+  aAll:　880,
+  kHalf:　636,
+  aHalf:　625,
+  FourSixHalf:　788,
+  bHalf:　765
 }
 
 const paperSizeShort = {
-  kAll:636,
-  aAll:625,
-  kHalf:469,
-  aHalf:440,
-  FourSixHalf:545,
-  bHalf:542
+  kAll:　636,
+  aAll:　625,
+  kHalf:　469,
+  aHalf:　440,
+  FourSixHalf:　545,
+  bHalf:　542
 }
