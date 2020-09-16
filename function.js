c96631f012dbf4ec0923d9d4ddfa221162b2ca52
hiromichi-code <hiromichi.s.617@gmail.com>
2020-09-16 13:30:52 +0900

    天地サイズを表示する関数を作成途中

--- function.js
+++ function.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const bookSize = document.querySelectorAll(".size");
+const value = document.querySelector(".size").textContent;
+const valueText = document.querySelector(".right>.out-two,.right>.out-five").textContent;
+
+bookSize.onclick = function(){
+  if(value === "A4"){
+    valueText = bookSizeHight["A4"];
+  }
+}
+
