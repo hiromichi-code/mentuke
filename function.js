4e1fb78ed6e12667c8eb639f003500d90535ddaf
hiromichi-code <hiromichi.s.617@gmail.com>
2020-09-17 12:34:21 +0900

    天地、小口共にサイズ設定の関数を定義完了

--- function.js
+++ function.js
@@ -1,28 +1,37 @@
 'use strict';
 
 const bookSize = document.querySelectorAll(".size");
-const valueText = document.querySelectorAll(".height");
+const valueHeight = document.querySelectorAll(".height");
+const valueWidth = document.querySelectorAll(".width");
 
 bookSize.forEach(element => {
   element.onclick = function(){
     let height = 0;
+    let width = 0;
     if(element.innerHTML === "A4"){
       height = bookSizeHight["A4"];
+      width  = bookSizeWidth["A4"];
     } else if(element.innerHTML === "A5"){
       height = bookSizeHight["A5"];
+      width  = bookSizeWidth["A5"];
     } else if(element.innerHTML === "A6"){
       height = bookSizeHight["A6"];
+      width  = bookSizeWidth["A6"];
     } else if(element.innerHTML === "B5"){
       height = bookSizeHight["B5"];
+      width  = bookSizeWidth["B5"];
     } else if(element.innerHTML === "四六判"){
       height = bookSizeHight["fourSix"];
+      width  = bookSizeWidth["fourSix"];
     } else if(element.innerHTML === "B6"){
       height = bookSizeHight["B6"];
-    } else {
-      height = 0;
-    }
-    valueText.forEach(ele => {
+      width  = bookSizeWidth["B6"];
+    } 
+    valueHeight.forEach(ele => {
       ele.textContent = height;
+    });
+    valueWidth.forEach(ele => {
+      ele.textContent = width;
     });
   }
 });
