58edb9f2515c65ef4caa60c3ab6bc2767d710405
hiromichi-code <hiromichi.s.617@gmail.com>
2020-09-24 07:23:04 +0900

    メンタリング後。1つ目のFunctionから全体の紙サイズを引っ張る方法を教わった。2つ目のFunction構築前の状態

--- function.js
+++ function.js
@@ -8,92 +8,111 @@ const left = document.querySelectorAll(".paper-size-left");
 const right = document.querySelectorAll(".paper-size-right");
 
 // サイズ選択に対する処理
+let selectBookSize = "";
+let leftLong = 0;
+let leftShort = 0;
+let rightLong = 0;
+let rightShort = 0;
 bookSize.forEach(bookSizeElement => {
   bookSizeElement.onclick = function(){
-    let height = 0;
-    let width = 0;
-    let left = 0;
-    let right = 0;
+    let height1 = 0;
+    let width1 = 0;
+    let left1 = 0;
+    let right1 = 0;
+    selectBookSize = bookSizeElement.innerHTML;
     if(bookSizeElement.innerHTML === "A4"){
-      height = bookSizeHight["A4"];
-      width  = bookSizeWidth["A4"];
-      left = paperSizeLeft["A4"];
-      right = paperSizeRight["A4"];
+      height1 = bookSizeHight["A4"];
+      width1  = bookSizeWidth["A4"];
+      leftLong = paperSizeLong["kAll"];
+      leftShort = paperSizeShort["kAll"];
+      rightLong = paperSizeLong["aAll"];
+      rightShort = paperSizeShort["aAll"];
+      left1 = paperSizeLeft["A4"];
+      right1 = paperSizeRight["A4"];
     } else if(bookSizeElement.innerHTML === "A5"){
-      height = bookSizeHight["A5"];
-      width  = bookSizeWidth["A5"];
-      left = paperSizeLeft["A5"];
-      right = paperSizeRight["A5"];
+      height1 = bookSizeHight["A5"];
+      width1  = bookSizeWidth["A5"];
+      leftLong = paperSizeLong["kAll"];
+      leftShort = paperSizeShort["kAll"];
+      rightLong = paperSizeLong["aAll"];
+      rightShort = paperSizeShort["aAll"];
+      left1 = paperSizeLeft["A5"];
+      right1 = paperSizeRight["A5"];
     } else if(bookSizeElement.innerHTML === "A6"){
-      height = bookSizeHight["A6"];
-      width  = bookSizeWidth["A6"];
-      left = paperSizeLeft["A6"];
-      right = paperSizeRight["A6"];
+      height1 = bookSizeHight["A6"];
+      width1  = bookSizeWidth["A6"];
+      leftLong = paperSizeLong["kAll"];
+      leftShort = paperSizeShort["kAll"];
+      rightLong = paperSizeLong["aAll"];
+      rightShort = paperSizeShort["aAll"];
+      left1 = paperSizeLeft["A6"];
+      right1 = paperSizeRight["A6"];
     } else if(bookSizeElement.innerHTML === "B5"){
-      height = bookSizeHight["B5"];
-      width  = bookSizeWidth["B5"];
-      left = paperSizeLeft["B5"];
-      right = paperSizeRight["B5"];
+      height1 = bookSizeHight["B5"];
+      width1  = bookSizeWidth["B5"];
+      leftLong = paperSizeLong["kAll"];
+      leftShort = paperSizeShort["kAll"];
+      rightLong = paperSizeLong["aAll"];
+      rightShort = paperSizeShort["aAll"];
+      left1 = paperSizeLeft["B5"];
+      right1 = paperSizeRight["B5"];
     } else if(bookSizeElement.innerHTML === "四六判"){
-      height = bookSizeHight["fourSix"];
-      width  = bookSizeWidth["fourSix"];
-      left = paperSizeLeft["fourSix"];
-      right = paperSizeRight["fourSix"];
+      height1 = bookSizeHight["fourSix"];
+      width1  = bookSizeWidth["fourSix"];
+      leftLong = paperSizeLong["kAll"];
+      leftShort = paperSizeShort["kAll"];
+      rightLong = paperSizeLong["aAll"];
+      rightShort = paperSizeShort["aAll"];
+      left1 = paperSizeLeft["fourSix"];
+      right1 = paperSizeRight["fourSix"];
     } else if(bookSizeElement.innerHTML === "B6"){
-      height = bookSizeHight["B6"];
-      width  = bookSizeWidth["B6"];
-      left = paperSizeLeft["B6"];
-      right = paperSizeRight["B6"];
+      height1 = bookSizeHight["B6"];
+      width1  = bookSizeWidth["B6"];
+      leftLong = paperSizeLong["kAll"];
+      leftShort = paperSizeShort["kAll"];
+      rightLong = paperSizeLong["aAll"];
+      rightShort = paperSizeShort["aAll"];
+      left1 = paperSizeLeft["B6"];
+      right1 = paperSizeRight["B6"];
     } 
     height.forEach(ele => {
-      ele.textContent = height;
+      ele.textContent = height1;
     });
     width.forEach(ele => {
-      ele.textContent = width;
+      ele.textContent = width1;
     });
     left.forEach(ele => {
-      ele.textContent = left;
+      ele.textContent = left1;
     });
     right.forEach(ele => {
-      ele.textContent = right;
+      ele.textContent = right1;
     });
-    // return bookSizeElement;
   }
 });
 
 
 // 製本方法処理に利用する定数
-// const methodBookBinding = document.querySelectorAll(".method");
-// const methodOutLeft = document.querySelectorAll(".method-out-left");
-// const methodInLeft = document.querySelectorAll(".method-in-left");
-// const methodShaveLeft = document.querySelectorAll(".method-shave-left");
-// const methodClipLeft = document.querySelectorAll(".method-clip-left");
-// const methodClipOutLeft = document.querySelectorAll(".method-clip-out-left");
-// const methodMarginLeft = document.querySelectorAll(".method-margin-left");
-// const methodOutRight = document.querySelectorAll(".method-out-right");
-// const methodInRight = document.querySelectorAll(".method-in-right");
-// const methodShaveRight = document.querySelectorAll(".method-shave-right");
-// const methodClipRight = document.querySelectorAll(".method-clip-right");
-// const methodClipOutRight = document.querySelectorAll(".method-clip-out-right");
-// const methodMarginRight = document.querySelectorAll(".method-margin-right");
+const methodBookBinding = document.querySelectorAll(".method");
+const methodOutLeft = document.querySelectorAll(".method-out-left");
+const methodInLeft = document.querySelectorAll(".method-in-left");
+const methodShaveLeft = document.querySelectorAll(".method-shave-left");
+const methodClipLeft = document.querySelectorAll(".method-clip-left");
+const methodClipOutLeft = document.querySelectorAll(".method-clip-out-left");
+const methodMarginLeft = document.querySelectorAll(".method-margin-left");
+const methodOutRight = document.querySelectorAll(".method-out-right");
+const methodInRight = document.querySelectorAll(".method-in-right");
+const methodShaveRight = document.querySelectorAll(".method-shave-right");
+const methodClipRight = document.querySelectorAll(".method-clip-right");
+const methodClipOutRight = document.querySelectorAll(".method-clip-out-right");
+const methodMarginRight = document.querySelectorAll(".method-margin-right");
 
-// methodBookBinding.forEach(methodElement => {
-//   methodElement.onclick = function(){
-//     let leftLong = 0;
-//     let LeftShort = 0;
-//     let rightLong = 0;
-//     let rightShort = 0;
-//     if(methodElement.innerHTML === "無線"){
-//       if(bookSizeElement === "A4"){
-//         let leftLong = paperSizeLong["kAll"];
-//         let LeftShort = paperSizeShort["kAll"];
-//         let rightLong = paperSizeLong["aALl"];
-//         let rightShort = paperSizeShort["aAll"];
-//         methodOutLeft
-//       }
-//     }
-//   }
-// });
+methodBookBinding.forEach(methodElement => {
+  methodElement.onclick = function(){
+    if(methodElement.innerHTML === "無線"){
+    
+    }
+  }
+});
 
 
 
